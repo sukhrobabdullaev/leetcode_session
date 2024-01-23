3565d62eb655d07d76248782eec0ff56fb1757e7
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-01-23 21:53:20 +0500

    arrays

---
+++ index.ts
@@ -0,0 +1,20 @@
+const arr1: number[] = [2, 1, 5, 4, 3, 5];
+const arr2: number[] = [1.3, -32, 3, -24, 5.4];
+let fruits: string[] = ["Banana", "Orange", "Apple", "Mango"];
+
+// console.log(arr1.concat(arr2.concat([2, 3, 2])));
+// console.log(arr1.indexOf(-1));
+// console.log(arr2.filter((el) => el > 0)); // filter
+// console.log(arr1.sort((a, b) => b - a)); // sort
+// console.log(arr1.every((el) => el < 3)); // false - all elements of array
+// console.log(arr1.some((el) => el < 3)); // true - any element of array
+// console.log(fruits.includes('Banana')); // true
+// console.log(arr1.find((el) => el > 1)); // 5 - first el
+// console.log(arr1.findIndex((el) => el > 1)); // 0
+// arr1.fill(0,2,4)
+// console.log(arr1); // [ 2, 1, 0, 0, 3, 5 ] with updating array
+// arr.slice(2,4) -  [5,4]
+// isArray
+// 
+
+
